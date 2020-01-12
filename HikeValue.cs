3d4d10741fc42978c0725eb997bba008ddccb57d
prax78 <prax78@gmail.com>
2020-01-12 17:22:26 +0530

    Add files via upload

---
+++ HikeValue.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+
+namespace HikeValue
+{
+    [Cmdlet(VerbsCommon.Show, "HikePercentToValue")]
+
+    public class HikeValue:PSCmdlet
+    {
+
+        [Parameter(Mandatory =true,Position =0)]
+        public double DesiredHikePercent { get; set; }
+        
+        [Parameter(Mandatory =true)]
+        public double CurrentSalary { get; set; }
+
+
+        protected override void ProcessRecord()
+        {
+            var getHike = CurrentSalary*DesiredHikePercent / 100;
+            var result = getHike + CurrentSalary;
+            WriteObject($"Your Increased Salry after a hike of {DesiredHikePercent} % Would be {result}");
+
+        }
+
+
+    }
+
+
+
+}
---
+++ SalaryPerccent.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+
+namespace SalaryPerccent
+{
+    [Cmdlet(VerbsCommon.Show, "MySalaryIncreasePercentage")]
+
+
+    public class SalaryPerccent : PSCmdlet
+    {
+        [Parameter(Mandatory = true, Position = 0, HelpMessage = "Enter Previous Salary to compute Salary Hike in Percentage")]
+        public double  PrevSalary { get; set; }
+
+        [Parameter(Mandatory = true, HelpMessage = "Enter Offered/Given Salary to compute Salary Hike in Percentage")]
+        public double  NewSalary { get; set; }
+
+        protected override void ProcessRecord()
+        {
+
+            double result = (NewSalary - PrevSalary )/ PrevSalary * 100;
+            var Cresult = result <= 10 ? "Your Salary Hike Sucks!! it is only " + result : result > 10 && result <= 25 ? "Your Salary Hike is Decent!!, it is " + result : result > 25 ? "Your Salary Hike is AweSome, it is " + result : "No Fun!!";
+            WriteObject(Cresult);
+
+
+        }
+
+    }
+}
